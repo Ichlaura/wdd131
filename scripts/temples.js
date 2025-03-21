9a1668bc47847e61ec9c0ed4dd424f8b85b53481
laura nunez <laupao41@gmail.com>
2025-03-21 16:45:38 +0900

    initial commit

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,7 +1,11 @@
+
+
+// Para actualizar el año actual y la fecha de la última modificación
 let d = new Date();
 document.getElementById("currentYear").innerHTML = `&copy;${d.getFullYear()}`;
 document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
 
+// Función para alternar clases 'show' (para el menú y el título)
 const hambutton = document.querySelector('#hambutton');
 
 hambutton.addEventListener('click', () => {
@@ -10,6 +14,24 @@ hambutton.addEventListener('click', () => {
   hambutton.classList.toggle('show');
 });
 
+// Función para manejar el menú de enlaces
+const menuLinks = document.querySelectorAll('#navmenu a');
+
+// Agregar el evento click y touchstart a cada enlace
+menuLinks.forEach(link => {
+  // Manejar el evento click (en escritorio)
+  link.addEventListener('click', function(event) {
+    toggleActive(this); // Cambiar el estado del enlace
+    event.preventDefault(); // Evitar el comportamiento por defecto
+  });
+
+  // Manejar el evento touchstart (en dispositivos táctiles)
+  link.addEventListener('touchstart', function(event) {
+    toggleActive(this); // Cambiar el estado del enlace
+  });
+});
+
+// Función para alternar la clase "active" en los enlaces
 function toggleActive(element) {
   element.classList.toggle("active");
 }
